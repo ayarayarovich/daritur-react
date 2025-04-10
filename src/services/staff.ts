8d98eeb5f459643db898e741bcdce64be5c718f8
Айяр Халилов <super.ayar-04@yandex.ru>
2025-04-10 08:12:53 +0300

    optional officeId for employee

--- src/services/staff.ts
+++ src/services/staff.ts
@@ -38,7 +38,7 @@ export const getEmployee = async (payload: { id: number }) => {
     lastName: z.string(),
     phone: z.string(),
     role: z.string(),
-    officeId: z.number(),
+    officeId: z.number().optional().default(0),
   })
   const data = schema.parse(response.data)
   return data
