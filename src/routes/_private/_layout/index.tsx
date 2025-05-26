13022878ac79e65f9258c714502715eb33face48
Айяр Халилов <super.ayar-04@yandex.ru>
2025-05-26 11:37:23 +0300

    uea

--- src/routes/_private/_layout/index.tsx
+++ src/routes/_private/_layout/index.tsx
@@ -1,9 +1,17 @@
-import { createFileRoute, Navigate } from '@tanstack/react-router'
+import toast from 'react-hot-toast'
+
+import { createFileRoute, redirect } from '@tanstack/react-router'
+
+import { Query } from '@/shared'
+import Queries from '@/shared/queries'
 
 export const Route = createFileRoute('/_private/_layout/')({
-  component: RouteComponent,
+  beforeLoad: async () => {
+    const menu = await Query.client.fetchQuery(Queries.me.menu)
+    const first = menu[0]
+    if (!first) {
+      toast.error('У вас нет доступов!')
+    }
+    throw redirect({ to: `/${first.path}` })
+  },
 })
-
-function RouteComponent() {
-  return <Navigate to='/staffs' />
-}
